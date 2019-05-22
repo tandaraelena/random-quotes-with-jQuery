e7672d6d64c79fc40071b0b34333dd5967d43504
Tandara Elena <tandaraelena@yahoo.com>
2019-05-22 13:53:07 +0100

    added the random quotes in js file

---
+++ script.js
@@ -0,0 +1,44 @@
+let quotes = [
+  {
+    quote:
+      "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.",
+    author: "Bernard M. Baruch"
+  },
+  {
+    quote:
+      "You know you're in love when you can't fall asleep because reality is finally better than your dreams.",
+    author: "Dr. Seuss"
+  },
+  {
+    quote: "You only live once, but if you do it right, once is enough.",
+    author: "Mae West"
+  },
+  {
+    quote:
+      "In three words I can sum up everything I've learned about life: it goes on.",
+    author: "Robert Frost"
+  },
+  {
+    quote: "If you tell the truth, you don't have to remember anything.",
+    author: "Mark Twain"
+  },
+  {
+    quote:
+      "I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.",
+    author: "Maya Angelou"
+  },
+  {
+    quote: "Always forgive your enemies; nothing annoys them so much.",
+    author: "Oscar Wilde"
+  },
+  {
+    quote:
+      "“Live as if you were to die tomorrow. Learn as if you were to live forever.",
+    author: "Mahatma Gandhi"
+  },
+  {
+    quote:
+      "I am so clever that sometimes I don't understand a single word of what I am saying.",
+    author: "Oscar Wilde"
+  }
+];
